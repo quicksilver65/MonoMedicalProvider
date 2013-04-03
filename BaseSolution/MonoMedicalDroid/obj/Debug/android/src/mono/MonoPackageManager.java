33ef22900810803fcdf97478c2f9bd072cb2294d
developer <developer@developers-Mac.local>
2013-04-02 22:12:45 -0700

    Android ReAdded

--- BaseSolution/MonoMedicalDroid/obj/Debug/android/src/mono/MonoPackageManager.java
+++ BaseSolution/MonoMedicalDroid/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -71,5 +71,5 @@ class MonoPackageManager_Resources {
 	};
 	public static final String[] Dependencies = new String[]{
 	};
-	public static final String ApiPackageName = "Mono.Android.Platform.ApiLevel_8";
+	public static final String ApiPackageName = "Mono.Android.Platform.ApiLevel_10";
 }
